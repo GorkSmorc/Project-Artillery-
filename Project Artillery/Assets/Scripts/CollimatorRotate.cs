c5974699088851e6941677e3df46e63ef42a4de2
domenik1 <gorn199789@gmail.com>
2019-06-19 16:43:07 +0300

    no message

---
+++ Project Artillery/Assets/Scripts/CollimatorRotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollimatorRotate : MonoBehaviour
+{
+    public Camera Camera;
+    private Vector3 CamTransform, TargetPosition;
+    public float rotationSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        CamTransform = Camera.gameObject.transform.position;
+        TargetPosition = CamTransform - this.gameObject.transform.position;
+        //TargetPosition.y = 0;
+        //TargetPosition.z = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = Quaternion.Angle(transform.localRotation, Quaternion.LookRotation(TargetPosition));
+        this.gameObject.transform.localRotation = Quaternion.Slerp(this.gameObject.transform.localRotation, Quaternion.LookRotation(TargetPosition), Mathf.Min(1f, Time.deltaTime * rotationSpeed / angle));
+    }
+}
